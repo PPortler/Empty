091d8415548833cf9ccd6c1b10291cc13efaceee
PPortler <phitakpong2546@gmail.com>
2024-02-08 22:10:33 +0700

    main & login

--- app/src/main/java/com/example/empty/Login.kt
+++ app/src/main/java/com/example/empty/Login.kt
@@ -1,11 +1,33 @@
 package com.example.empty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextPaint
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 
 class Login : AppCompatActivity() {
+    var login_button_back:ImageButton? = null
+    var login_edit_user:EditText? = null
+    var login_edit_password:EditText? = null
+    var login_button_login:Button? = null
+    var login_button_forgot:Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        init()
+        login_button_back?.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun init(){
+        login_button_back = findViewById(R.id.login_button_back)
+        login_edit_user = findViewById(R.id.login_edit_user)
+        login_edit_password = findViewById(R.id.login_edit_password)
+        login_button_login = findViewById(R.id.login_button_login)
+        login_button_forgot = findViewById(R.id.login_button_forgot)
     }
 }
--- app/src/main/java/com/example/empty/MainActivity.kt
+++ app/src/main/java/com/example/empty/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.empty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    var main_button_login: Button? = null
+    var main_button_create: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        main_button_login?.setOnClickListener {
+            var intent = Intent(this,Login::class.java)
+            startActivity(intent)
+        }
+    }
+    fun init(){
+        main_button_login = findViewById(R.id.main_button_login)
+        main_button_create = findViewById(R.id.main_button_create)
     }
 }
